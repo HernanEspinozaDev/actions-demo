3c477a70d761813e86f60257ed97ac52990c6aba
Hernán Espinoza <hernan.espinoza.dev@gmail.com>
2025-06-16 22:12:03 -0400

    Añadida nueva prueba para CI

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
-const sum = require('./sum'); 
+const sum = require('./sum');
 
-test('Suma dos números', () => {  
-  expect(sum(1, 2)).toBe(3); 
+test('Suma dos números', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+// NUEVA PRUEBA
+test('Suma dos números negativos', () => {
+  expect(sum(-1, -2)).toBe(-3);
 });
